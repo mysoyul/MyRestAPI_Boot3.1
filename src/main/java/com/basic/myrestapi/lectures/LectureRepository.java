33ebe0e48ba7fceb694e89249d76e30f9b7d9b96
mysoyul <vega2k_@naver.com>
2023-08-08 14:24:36 +0900

    repository finder method add

--- src/main/java/com/basic/myrestapi/lectures/LectureRepository.java
+++ src/main/java/com/basic/myrestapi/lectures/LectureRepository.java
@@ -1,5 +1,9 @@
 package com.basic.myrestapi.lectures;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface LectureRepository extends JpaRepository<Lecture, Integer> {
+    List<Lecture> findByName(String name);
 }
